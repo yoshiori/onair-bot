8054de1661da0127e11aeb123af6b5e70ab455be
Yoshiori Shoji <yoshiori@gmail.com>
2024-03-05 17:15:53 +0900

    bundle gem -b

---
+++ spec/onair/bot_spec.rb
@@ -0,0 +1,11 @@
+# frozen_string_literal: true
+
+RSpec.describe Onair::Bot do
+  it "has a version number" do
+    expect(Onair::Bot::VERSION).not_to be nil
+  end
+
+  it "does something useful" do
+    expect(false).to eq(true)
+  end
+end
